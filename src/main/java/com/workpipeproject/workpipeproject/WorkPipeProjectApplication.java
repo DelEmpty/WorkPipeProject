3dee6f59c5ee64b3b338180f5937a4884c5957f0
Mattia <mattiagamek@gmail.com>
2024-11-06 13:28:40 +0100

    Initial commit

---
+++ src/main/java/com/workpipeproject/workpipeproject/WorkPipeProjectApplication.java
@@ -0,0 +1,13 @@
+package com.workpipeproject.workpipeproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkPipeProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkPipeProjectApplication.class, args);
+    }
+
+}
